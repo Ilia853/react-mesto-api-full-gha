55a048911bbbaa175ac2fa7155c92b1cbbbceae8
Ilia Egorov <i.egorov119@yandex.ru>
2023-06-26 21:02:36 +0400

    deploy

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const routes = require('./routes/index');
 const { errorLogger } = require('./middlewares/logger');
 const { cors } = require('./middlewares/cors');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
